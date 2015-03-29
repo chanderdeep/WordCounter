12f04f8eaf0717e2e83d296e5d027959a6116285
chanderdeep <ch.yadav430@gmail.com>
2015-03-29 14:04:09 +1000

    initial commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mayank on 29/3/2015.
+ */
+public class App {
+    public static void main(String[] args){
+        String text = TextReader.load("simple.txt");
+        System.out.println(text);
+    }
+}
